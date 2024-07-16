3a3f86f8e4abc1177937d599ccc98c708d2f20de
psum <sum.philipp@gmail.com>
2024-07-16 20:09:11 +0200

    initial commit
    started tutorial and implemented basic functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
