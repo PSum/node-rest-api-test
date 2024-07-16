828413daf9a17ed6598331ec41148b44bb7d012d
psum <sum.philipp@gmail.com>
2024-07-16 20:48:46 +0200

    update

--- app.js
+++ app.js
@@ -3,22 +3,63 @@ const app = express();
 
 // https://www.robinwieruch.de/node-express-server-rest-api/
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
 });
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
 });
