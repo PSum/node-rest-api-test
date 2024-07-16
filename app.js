d55507e607d9b39162a8060f84b9c651f22f6fb9
psum <sum.philipp@gmail.com>
2024-07-16 20:09:59 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+// https://www.robinwieruch.de/node-express-server-rest-api/
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
