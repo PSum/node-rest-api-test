b2e58436a364639c21845b92d80b36cd21bb9499
PSum <sum.philipp@gmail.com>
2024-07-20 21:12:54 +0200

    update
    everything working with the example of the top website

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
-console.log('Hello Node.js project.');
+import models from './models';
+import routes from './routes';
 
-console.log(process.env.MY_SECRET);
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
